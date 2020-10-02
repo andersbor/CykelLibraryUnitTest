7ad2e1109415d58642ce1ff33d9fa87798eb23ec
Anders Børjesson <anbo@zealand.dk>
2020-10-02 15:20:56 +0200

    Add project files.

---
+++ CykelLibraryMedUnitTest/Cykel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace CykelLibraryMedUnitTest
+{
+    public class Cykel
+    {
+        private string _farve;
+        private int _pris;
+        private int _gear;
+
+        public int Id { get; set; }
+
+        public string Farve
+        {
+            get => _farve;
+            set
+            {
+                if (value == null) throw new ArgumentNullException();
+                if (value.Length < 1) throw new ArgumentException();
+                _farve = value;
+            }
+        }
+
+        public int Pris
+        {
+            get => _pris;
+            set
+            {
+                if (value <= 0) throw new ArgumentOutOfRangeException();
+                _pris = value;
+            }
+        }
+
+        public int Gear
+        {
+            get => _gear;
+            set
+            {
+                if (3 <= value && value <= 32) _gear = value;
+                else throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public override string ToString()
+        {
+            return Id + " " + Farve + " " + Pris + " " + Gear;
+        }
+    }
+}
---
+++ CykelLibraryMedUnitTestTests/CykelTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CykelLibraryMedUnitTest.Tests
+{
+    [TestClass]
+    public class CykelTests
+    {
+        private readonly Cykel _cykel = new Cykel();
+
+        /*[TestMethod]
+        public void TestConstructor()
+        {
+            Assert.IsTrue(_cykel.Farve.Length >= 1);
+            Assert.IsTrue(_cykel.Pris > 0);
+            Assert.IsTrue(3 <= _cykel.Gear && _cykel.Gear <= 32);
+        }*/
+
+        [TestMethod]
+        public void TestFarve()
+        {
+            _cykel.Farve = "A";
+            Assert.AreEqual("A", _cykel.Farve);
+            Assert.ThrowsException<ArgumentException>(() => _cykel.Farve = "");
+            Assert.ThrowsException<ArgumentNullException>(() => _cykel.Farve = null);
+        }
+
+        [TestMethod]
+        public void TestPris()
+        {
+            _cykel.Pris = 1;
+            Assert.AreEqual(1, _cykel.Pris);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _cykel.Pris = 0);
+        }
+
+        [TestMethod]
+        public void TestGear()
+        {
+            _cykel.Gear = 3;
+            Assert.AreEqual(3, _cykel.Gear);
+            _cykel.Gear = 32;
+            Assert.AreEqual(32, _cykel.Gear);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _cykel.Gear = 2);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _cykel.Gear = 33);
+        }
+
+        [TestMethod]
+        public void ToStringTest()
+        {
+            // TODO should test ...
+        }
+    }
+}
